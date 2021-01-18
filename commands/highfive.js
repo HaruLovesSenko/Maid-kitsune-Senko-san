4c28ccee86a95707d65330989822aa916faba2d0
Glitch (zenith-positive-marquis) <none>
2021-01-18 14:13:43 +0000

    🛥📯 Updated with Glitch

--- commands/highfive.js
+++ commands/highfive.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 const command = require('/app/exports/commandClass.js');
 const tenor = require('/app/exports/roleplay.js');
 
-module.exports = class hug extends command {
+module.exports = class highfive extends command {
   constructor() {
-    super("Hug your closest friends! 🤗", "s?hug [user]", "roleplay")
+    super("Highfive your friends! (。・∀・)ノヽ(・∀・ )", "s?highfive [user]", "roleplay")
   }
   
   run(msg, client, using) {
@@ -17,13 +17,9 @@ module.exports = class hug extends command {
       u = "no"
     }
     let arr = [
-      `${a} is hugging ${u}!`,
-      `${a} is holding ${u} very thightly!`,
-      `${a} is placing their arms around ${u}!`,
-      `${a} is squeezing ${u}!`,
-      `${a} is cuddling with ${u}!`
+      `${a} high fived ${u}`
     ]
     let msg2 = arr[Math.floor(Math.random()*arr.length)]
-    new tenor("anime-hug").sendGif(msg, msg2 + " 🤗", user, "hug")
+    new tenor("anime-highfive").sendGif(msg, msg2 + " (。・∀・)ノヽ(・∀・ )", user, "highfive")
   }
 }
